6c1f1f744b5d492029a3e56ef59e019bb43d4a92
TimboFimbo <oscarlangford@protonmail.com>
2023-09-03 22:04:16 +0100

    Added option to use SFTP with recursive mode on, along with connection settings for an additional Pi

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # RaspberryPi_QuickConnect
 For quickly connecting to any Raspberry Pi on the network
 
-Start with no args to SSH or with -f to SFTP
+Start with no args to SSH
+Start with -f to use SFTP
+Start with -fr to use SFTP with Recursive mode on
 
-Add script to path with .sh removed from name for easy access
+Add script to path for easy access (you can remove the .sh from script name)
+
+TODO:
+    * Add a dictionary or class to hold the Pi info, rather than
+      having it as hard-coded strings throughout the script
--- rpi_connect.sh
+++ rpi_connect.sh
@@ -4,29 +4,39 @@ pi_a_con="timbo@192.168.0.116"
 pi_b_con="timbo@192.168.0.117"
 pi_c_con="timbo@192.168.0.118"
 pi_d_con="timbo@192.168.0.119"
+pi_e_con="timbo@192.168.0.120"
 
 selected_pi=$pi_a_con
 
 use_sftp=false
+use_sftpr=false
 for inp in "$@"
 do
-    if [ "$inp" = "-f" ]
+    if [ "$inp" = "-fr" ]
+    then
+        use_sftpr=true
+    elif [ "$inp" = "-f" ]
     then
         use_sftp=true
     fi
 done
 
-if [ "$use_sftp" = true ]
+if [ "$use_sftpr" = true ]
+then
+    echo -e "\nEnter a letter to SFTP into Raspberry Pi, with Recursive mode on\n"
+elif [ "$use_sftp" = true ]
 then
     echo -e "\nEnter a letter to SFTP into Raspberry Pi\n"
 else
-    echo -e "\nEnter a letter to SSH into Raspberry Pi (restart with -f to SFTP instead)\n"
+    echo -e "\nEnter a letter to SSH into Raspberry Pi"
+    echo -e "You can also restart with -f to use SFTP, or -fr to use SFTP with Recursive mode on\n"
 fi
 
 echo "  a : AngelPi       | Pi 2        | Raspberry Pi OS Lite (32-bit)"
 echo "  b : BostonCreamPi | Pi Zero W   | Raspberry Pi OS Lite (32-bit)"
 echo "  c : ChickenPotPi  | Pi 4 (4GB)  | Raspberry Pi OS Lite (64-bit)"
 echo "  d : DerbyPi       | Pi Zero 2 W | Raspberry Pi OS Lite (32-bit)"
+echo "  e : EskimoPi      | Pi 4 (8GB)  | Raspberry Pi OS (64-bit)"
 
 read letter
 
@@ -45,12 +55,19 @@ elif [ "$letter" = 'd' ] || [ "$letter" = 'D' ]
 then
     echo -e "\nConnecting to DerbyPi..."
     selected_pi=$pi_d_con
+elif [ "$letter" = 'e' ] || [ "$letter" = 'E' ]
+then
+    echo -e "\nConnecting to EskimoPi..."
+    selected_pi=$pi_e_con
 else
     echo -e "\nNo matching Pi. Quitting"
     exit 0
 fi
 
-if [ "$use_sftp" = true ]
+if [ "$use_sftpr" = true ]
+then
+    sftp -r $selected_pi
+elif [ "$use_sftp" = true ]
 then
     sftp $selected_pi
 else
