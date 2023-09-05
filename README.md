57e9303258f835a5702ec31a06b3afcec988257f
TimboFimbo <oscarlangford@protonmail.com>
2023-09-05 15:31:03 +0100

    Almost entirely rewritten: All Pi info is now contained in arrays at the top of the script, rather than sprinkled throughout, and the connection table is dynamically sized, rather than being hardcoded. This allows new Pis to be added much more easily, but the code could still use some optimization

--- README.md
+++ README.md
@@ -5,8 +5,9 @@ Start with no args to SSH
 Start with -f to use SFTP
 Start with -fr to use SFTP with Recursive mode on
 
-Add script to path for easy access (you can remove the .sh from script name)
+Add script to $PATH for easy access (you can remove the .sh from script name)
 
-TODO:
-    * Add a dictionary or class to hold the Pi info, rather than
-      having it as hard-coded strings throughout the script
+Note: As this script is just issuing shell commands, it made sense to write it
+      in Bash. However, Bash turned out to be much more fiddly than I hoped,
+      so even though the script works, it would probably make even a 
+      moderately-competent coder weep. I'll improve it as I learn more.
--- rpi_connect.sh
+++ rpi_connect.sh
@@ -1,12 +1,74 @@
 #!/bin/bash
 
-pi_a_con="timbo@192.168.0.116"
-pi_b_con="timbo@192.168.0.117"
-pi_c_con="timbo@192.168.0.118"
-pi_d_con="timbo@192.168.0.119"
-pi_e_con="timbo@192.168.0.120"
+## Add as many Pis as you need here,
+## then add their variable names to the list below
+declare -A Pi_A=( 
+    [ip_address]="192.168.0.116" 
+    [con_code]="a" 
+    [username]="timbo" 
+    [pi_name]="AngelPi" 
+    [pi_model]="Pi_2" 
+    [pi_os]="Raspberry_Pi_OS_Lite_(32-bit)"
+    [pi_info]="Online_Radio_Streamer" 
+    )
 
-selected_pi=$pi_a_con
+declare -A Pi_B=( 
+    [ip_address]="192.168.0.117" 
+    [con_code]="b" 
+    [username]="timbo" 
+    [pi_name]="BostonCreamPi" 
+    [pi_model]="Pi_Zero_W" 
+    [pi_os]="Raspberry_Pi_OS_Lite_(32-bit)"
+    [pi_info]="" 
+    )
+
+declare -A Pi_C=( 
+    [ip_address]="192.168.0.118" 
+    [con_code]="c" 
+    [username]="timbo" 
+    [pi_name]="ChickenPotPi" 
+    [pi_model]="Pi_4_(4GB)" 
+    [pi_os]="Raspberry_Pi_OS_Lite_(64-bit)"
+    [pi_info]="Jellyfin_Media_Server" 
+    )
+
+declare -A Pi_D=( 
+    [ip_address]="192.168.0.119" 
+    [con_code]="d" 
+    [username]="timbo" 
+    [pi_name]="DerbyPi" 
+    [pi_model]="Pi_Zero_2_W" 
+    [pi_os]="Raspberry_Pi_OS_Lite_(32-bit)"
+    [pi_info]="" 
+    )
+
+declare -A Pi_E=( 
+    [ip_address]="192.168.0.120" 
+    [con_code]="e" 
+    [username]="timbo" 
+    [pi_name]="EskimoPi" 
+    [pi_model]="Pi_4_(8GB)" 
+    [pi_os]="Raspberry_Pi_OS_(64-bit)"
+    [pi_info]="" 
+    )
+
+## Add the variable names to this list
+declare pi_list=( Pi_A Pi_B Pi_C Pi_D Pi_E )
+
+## Do not delete this one - just keep it blank
+## There's probably a better way of doing this,
+## I just haven't spent the time to look into it
+declare -A selected_pi=( 
+    [ip_address]="" 
+    [con_code]="" 
+    [username]="" 
+    [pi_name]="" 
+    [pi_model]="" 
+    [pi_os]=""
+    [pi_info]="" 
+    )
+
+declare unformatted_output=""
 
 use_sftp=false
 use_sftpr=false
@@ -23,54 +85,74 @@ done
 
 if [ "$use_sftpr" = true ]
 then
-    echo -e "\nEnter a letter to SFTP into Raspberry Pi, with Recursive mode on\n"
+    echo -e "\nEnter a letter to SFTP into Raspberry Pi, with Recursive mode on"
 elif [ "$use_sftp" = true ]
 then
-    echo -e "\nEnter a letter to SFTP into Raspberry Pi\n"
+    echo -e "\nEnter a letter to SFTP into Raspberry Pi"
 else
     echo -e "\nEnter a letter to SSH into Raspberry Pi"
-    echo -e "You can also restart with -f to use SFTP, or -fr to use SFTP with Recursive mode on\n"
+    echo -e "You can also restart with -f to use SFTP, or -fr to use SFTP with Recursive mode on"
 fi
 
-echo "  a : AngelPi       | Pi 2        | Raspberry Pi OS Lite (32-bit)"
-echo "  b : BostonCreamPi | Pi Zero W   | Raspberry Pi OS Lite (32-bit)"
-echo "  c : ChickenPotPi  | Pi 4 (4GB)  | Raspberry Pi OS Lite (64-bit)"
-echo "  d : DerbyPi       | Pi Zero 2 W | Raspberry Pi OS Lite (32-bit)"
-echo "  e : EskimoPi      | Pi 4 (8GB)  | Raspberry Pi OS (64-bit)"
+echo ""
+for pi in "${pi_list[@]}"
+do
+    declare -n this_pi="$pi"
+    unformatted_output+=" | ""${this_pi[con_code]}"" | "
+    unformatted_output+="${this_pi[pi_name]}"" | "
+    unformatted_output+="${this_pi[ip_address]}"" | "
+    unformatted_output+="${this_pi[pi_model]}"" | "
+    unformatted_output+="${this_pi[pi_os]}"" | "
+    if [ ! -z "${this_pi[pi_info]}" ]
+    then
+        unformatted_output+="${this_pi[pi_info]}"" | "
+    fi
+    unformatted_output+=$'\n'
+done
+echo "$unformatted_output" | column -t
+echo ""
+
+pi_found=false
 
 read letter
 
-if [ "$letter" = 'a' ] || [ "$letter" = 'A' ]
+# Could probably do this loop better, without expicitly mentioning variable names
+for pi in "${pi_list[@]}"
+do
+    declare -n this_pi="$pi"
+    if [ $pi_found=false ]
+    then
+        if [ "$letter" = "${this_pi[con_code]}" ]
+        then
+            selected_pi[con_code]="${this_pi[con_code]}"
+            selected_pi[username]="${this_pi[username]}"
+            selected_pi[ip_address]="${this_pi[ip_address]}"
+            selected_pi[pi_name]="${this_pi[pi_name]}"
+            selected_pi[pi_model]="${this_pi[pi_model]}"
+            selected_pi[pi_os]="${this_pi[pi_os]}"
+            selected_pi[pi_info]="${this_pi[pi_info]}"
+            pi_found=true
+        fi
+    fi
+done
+
+if [ "$pi_found" = false ]
 then
-    echo -e "\nConnecting to AngelPi..."
-elif [ "$letter" = 'b' ] || [ "$letter" = 'B' ]
-then
-    echo -e "\nConnecting to BostonCreamPi..."
-    selected_pi=$pi_b_con
-elif [ "$letter" = 'c' ] || [ "$letter" = 'C' ]
-then
-    echo -e "\nConnecting to ChickenPotPi..."
-    selected_pi=$pi_c_con
-elif [ "$letter" = 'd' ] || [ "$letter" = 'D' ]
-then
-    echo -e "\nConnecting to DerbyPi..."
-    selected_pi=$pi_d_con
-elif [ "$letter" = 'e' ] || [ "$letter" = 'E' ]
-then
-    echo -e "\nConnecting to EskimoPi..."
-    selected_pi=$pi_e_con
-else
     echo -e "\nNo matching Pi. Quitting"
     exit 0
 fi
 
+echo -e "\nConnecting to ""${selected_pi[pi_name]}""..."
+
+connection_string="${selected_pi[username]}""@""${selected_pi[ip_address]}"
+
 if [ "$use_sftpr" = true ]
 then
-    sftp -r $selected_pi
+    sftp -r $connection_string
 elif [ "$use_sftp" = true ]
 then
-    sftp $selected_pi
+    sftp $connection_string
 else
-    ssh $selected_pi
+    ssh $connection_string
 fi
 exit 0
